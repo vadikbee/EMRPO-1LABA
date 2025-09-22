cc100e4d36466dab4f54d3329a9f7951f7b04ef8
vadikbee <65399822+vadikbee@users.noreply.github.com>
2025-09-22 20:10:57 +0300

    2223

--- EMRPO-LABA1.py
+++ EMRPO-LABA1.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 from scipy.optimize import fsolve
-from scipy.special import factorial # Импорт остается для таблицы 24242424
+from scipy.special import factorial # Импорт остается для таблицы 24242424333
 from io import StringIO
 
 # --- Конфигурация страницы ---
